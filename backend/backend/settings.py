f78745b3c5953e29383a8081cae4ec1c612db247
João Marcello Amaral <marcello.jao1611@gmail.com>
2025-11-08 17:04:00 -0300

    Criação dos apps games e review

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -120,3 +122,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+load_dotenv()
+
+IGDB_CLIENT_ID = os.getenv("IGDB_CLIENT_ID")
+IGDB_CLIENT_SECRET = os.getenv("IGDB_CLIENT_SECRET")
---
+++ backend/games/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class Game(models.Model):
+    id = models.IntegerField(unique=True)
+    name = models.CharField(max_length=100)
+    genres = models.JSONField(default=list)
+    age_ratings = models.JSONField(default=list)
+    summary = models.TextField(blank=True, null=True)
+    release_date = models.DateField(blank=True, null=True)
+    cover_url = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
